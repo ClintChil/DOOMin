057f91d192b6a0a938be331c03704b2691068f0b
Clint Chilcott <clintchil@gmail.com>
2016-02-29 03:44:53 -0800

    create EmojiMan with Haunt Behavior

---
+++ Assets/Haunt.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Haunt : MonoBehaviour {
+	public GameObject targetPlayer;
+	public float speed = 1.0f;
+
+	void Start() {
+		LookAtTarget();
+	}
+
+	void Update() {
+		LookAtTarget();
+		MoveTowardsTarget();
+	}
+
+	void LookAtTarget() {
+		//FIXME find a less CPU intensive way of billboarding sprite?
+		//FIXME make sprite look at player camera. Sprite is looking at player's belly.
+		transform.LookAt(targetPlayer.transform.position, targetPlayer.transform.up);
+	}
+
+	void MoveTowardsTarget() {
+		transform.Translate(0, 0, speed * Time.deltaTime);
+	}
+}
--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class SceneController : MonoBehaviour {
 	[SerializeField] private GameObject enemyPrefab;
+	[SerializeField] private GameObject emojiManPrefab;
 	private GameObject _enemy;
+	private GameObject _emojiMan;
 
 	void Start () {
 	
@@ -16,5 +18,10 @@ public class SceneController : MonoBehaviour {
 			float angle = Random.Range(0, 360);
 			_enemy.transform.Rotate(0, angle, 0);
 		}
+
+//		if(_emojiMan == null) {
+//			_emojiMan = Instantiate(emojiManPrefab) as GameObject;
+//			_emojiMan.transform.position = new Vector3(0, 4, 0);
+//		}
 	}
 }
