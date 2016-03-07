b7c4f9e4c73bd01e2c2c82bab9b155eb4d3a1b41
Clint Chilcott <clintchil@gmail.com>
2016-03-07 11:13:30 -0800

    add Settings UI

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsPopup : MonoBehaviour {
+	[SerializeField] private Slider speedSlider;
+
+	public void Open() {
+		gameObject.SetActive(true);
+	}
+
+	public void Close() {
+		gameObject.SetActive(false);
+	}
+
+	public void OnSubmitName(string name) {
+		Debug.Log(name);
+	}
+
+	public void OnSpeedValue(float speed) {
+		Messenger<float>.Broadcast(GameEvent.SPEED_CHANGED, speed);
+		Debug.Log("Speed: " + speed);
+		PlayerPrefs.SetFloat("speed", speed);
+	}
+
+	void Start() {
+		speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
+	}
+}
