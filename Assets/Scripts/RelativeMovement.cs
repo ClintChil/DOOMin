5f56ca00dcf8f16175fcd4db2d8424f871dc600b
Clint Chilcott <clintchil@gmail.com>
2016-03-08 12:14:54 -0800

    add trigger based door

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -10,6 +10,7 @@ public class RelativeMovement : MonoBehaviour {
 	public float gravity = -9.8f;
 	public float terminalVelocity = -10.0f;
 	public float minFall = -1.5f;
+	public float pushForce = 3.0f;
 
 	private float _vertSpeed;
 	private ControllerColliderHit _contact;
@@ -81,5 +82,11 @@ public class RelativeMovement : MonoBehaviour {
 	}
 	void OnControllerColliderHit(ControllerColliderHit hit) {
 		_contact = hit;
+
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body != null && !body.isKinematic) {
+			body.velocity = hit.moveDirection * pushForce;
+		}
+
 	}
 }
