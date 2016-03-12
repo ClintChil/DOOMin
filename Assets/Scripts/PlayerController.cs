85503dccddfc968ac89f860a6def57a69ee7360f
Clint Chilcott <clintchil@gmail.com>
2016-03-11 18:31:47 -0800

    add touch control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,20 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void FixedUpdate() {
-		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		moveDirection = Camera.main.transform.TransformDirection(moveDirection);
-		moveDirection.y = 0;
-		_rigidbody.AddForce(moveDirection * speed);
+
+		if (Input.touchCount > 0) {
+			Vector2 touchDelta = Input.GetTouch(0).deltaPosition;
+			Vector3 moveDirection = new Vector3(touchDelta.x, 0, touchDelta.y);
+
+			moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+			moveDirection.y = 0;
+
+			_rigidbody.AddForce(moveDirection * speed);
+		} 
+
+//		Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+
+
 	}
 }
