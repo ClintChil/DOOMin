141380773b41e600a7efb68a6d95244612a2a7e5
Clint Chilcott <clintchil@gmail.com>
2016-04-09 09:03:27 -0700

    [wip] tweaking physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,30 +15,36 @@ public class PlayerController : MonoBehaviour {
 		for (int i = 0; i < count; i++) {
 			Vector3 touchDeltaPosition = Input.GetTouch(i).deltaPosition;
 			if (Input.touchCount > 0) {
-				if (touchDeltaPosition.x > 0) {
-					_rigidbody.AddForce(Vector3.right * speed * Time.deltaTime * speed);
-					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
-						_rigidbody.AddForce(Vector3.right * speed * Time.deltaTime * speed);
-					}
-				}
-				if(touchDeltaPosition.x < 0) {
-					_rigidbody.AddForce(-Vector3.right * speed * Time.deltaTime * speed);
-					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
-						_rigidbody.AddForce(-Vector3.right * speed * Time.deltaTime * speed);
-					}
-				}
-				if (touchDeltaPosition.y > 0) {
-					_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime * speed);
-					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
-						_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime * speed);
-					}
-				}
-				if (touchDeltaPosition.y < 0) {
-					_rigidbody.AddForce(-Vector3.forward * speed * Time.deltaTime * speed);
-					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
-						_rigidbody.AddForce(-Vector3.forward * speed * Time.deltaTime * speed);
-					}
-				}
+
+				Vector3 moveDirection = new Vector3 (touchDeltaPosition.x, 0, touchDeltaPosition.y);
+				moveDirection.y = 0;
+
+				_rigidbody.AddForce (moveDirection * speed);
+
+//				if (touchDeltaPosition.x > 0) {
+//					_rigidbody.AddForce(Vector3.right * speed * Time.deltaTime * speed);
+//					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
+//						_rigidbody.AddForce(Vector3.right * speed * Time.deltaTime * speed);
+//					}
+//				}
+//				if(touchDeltaPosition.x < 0) {
+//					_rigidbody.AddForce(-Vector3.right * speed * Time.deltaTime * speed);
+//					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
+//						_rigidbody.AddForce(-Vector3.right * speed * Time.deltaTime * speed);
+//					}
+//				}
+//				if (touchDeltaPosition.y > 0) {
+//					_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime * speed);
+//					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
+//						_rigidbody.AddForce(Vector3.forward * speed * Time.deltaTime * speed);
+//					}
+//				}
+//				if (touchDeltaPosition.y < 0) {
+//					_rigidbody.AddForce(-Vector3.forward * speed * Time.deltaTime * speed);
+//					if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
+//						_rigidbody.AddForce(-Vector3.forward * speed * Time.deltaTime * speed);
+//					}
+//				}
 			}
 		}
 	}
